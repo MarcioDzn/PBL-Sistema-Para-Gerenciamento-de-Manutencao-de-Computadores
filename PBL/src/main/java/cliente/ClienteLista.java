6927c2ae3ba1846af4ae7f2c93c5e895e1821595
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-03-12 14:32:57 -0300

    feat: Classe ClienteLista
    DAO da classe Cliente referente a permanência de dados em uma lista

---
+++ PBL/src/main/java/cliente/ClienteLista.java
@@ -0,0 +1,37 @@
+package cliente;
+
+import model.Cliente;
+
+import java.util.List;
+
+public class ClienteLista implements ClienteDAO{
+    @Override
+    public Cliente buscarPorNome(String nome) {
+        return null;
+    }
+
+    @Override
+    public void adicionarNaLista(Cliente objeto) {
+
+    }
+
+    @Override
+    public Cliente buscarPorId(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public void atualizar(Cliente objeto) {
+
+    }
+
+    @Override
+    public void remover(Cliente objeto) {
+
+    }
+}
