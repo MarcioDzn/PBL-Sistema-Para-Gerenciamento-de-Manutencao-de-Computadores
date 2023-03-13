3e6abe3e8619d5f506fbc5b5d7e400c19d73fddb
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-03-13 16:14:01 -0300

    feat: Classe ServicoLista

---
+++ PBL/src/main/java/dao/servico/ServicoLista.java
@@ -0,0 +1,33 @@
+package dao.servico;
+
+import model.Servico;
+
+import java.util.List;
+
+public class ServicoLista implements ServicoDAO{
+
+    @Override
+    public void adicionarNaLista(Servico objeto) {
+
+    }
+
+    @Override
+    public Servico buscarPorId(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Servico> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public void atualizar(Servico objeto) {
+
+    }
+
+    @Override
+    public void remover(Servico objeto) {
+
+    }
+}
