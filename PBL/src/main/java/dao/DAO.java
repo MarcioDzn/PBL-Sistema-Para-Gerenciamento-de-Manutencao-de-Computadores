b16c8b20b66fbf1188605581f811ba808376d579
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-03-13 16:05:55 -0300

    feat: Adicionada classe OrdemServicoDAO no DAO

--- PBL/src/main/java/dao/DAO.java
+++ PBL/src/main/java/dao/DAO.java
@@ -2,9 +2,13 @@ package dao;
 
 import dao.cliente.ClienteDAO;
 import dao.cliente.ClienteLista;
+import dao.ordemServico.OrdemServicoDAO;
+import dao.ordemServico.OrdemServicoLista;
 
 public class DAO {
     private static ClienteDAO clienteDAO;
+
+    private static OrdemServicoDAO ordemServicoDAO;
 
     public static ClienteDAO getCliente(){
         if(clienteDAO == null){
@@ -12,5 +16,13 @@ public class DAO {
         }
 
         return clienteDAO;
+    }
+
+    public static OrdemServicoDAO getOrdemServico(){
+        if (ordemServicoDAO == null){
+            ordemServicoDAO = new OrdemServicoLista();
+        }
+
+        return ordemServicoDAO;
     }
 }
