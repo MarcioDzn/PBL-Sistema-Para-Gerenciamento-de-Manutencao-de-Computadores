61ce5ad360e38ddb73783a33de62d6f3b884df1a
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-03-13 16:51:18 -0300

    feat: Classe PecaLista

---
+++ PBL/src/main/java/dao/peca/PecaLista.java
@@ -0,0 +1,37 @@
+package dao.peca;
+
+import model.Peca;
+
+import java.util.List;
+
+public class PecaLista implements PecaDAO{
+    @Override
+    public void adicionarNaLista(Peca objeto) {
+        
+    }
+
+    @Override
+    public Peca buscarPorId(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Peca> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public void atualizar(Peca objeto) {
+
+    }
+
+    @Override
+    public void remover(Peca objeto) {
+
+    }
+
+    @Override
+    public List<Peca> buscarPorNome(String nome) {
+        return null;
+    }
+}
