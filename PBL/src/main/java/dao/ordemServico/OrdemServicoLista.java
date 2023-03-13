827acb4c44d3e8927cdf4cffd4e24bdd3bbca757
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-03-13 15:28:26 -0300

    feat: Classe OrdemServicoLista

---
+++ PBL/src/main/java/dao/ordemServico/OrdemServicoLista.java
@@ -0,0 +1,38 @@
+package dao.ordemServico;
+
+import model.OrdemServico;
+
+import java.util.List;
+
+public class OrdemServicoLista implements OrdemServicoDAO{
+
+    @Override
+    public void adicionarNaLista(OrdemServico objeto) {
+        
+    }
+
+    @Override
+    public OrdemServico buscarPorId(int id) {
+        return null;
+    }
+
+    @Override
+    public List<OrdemServico> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public void atualizar(OrdemServico objeto) {
+
+    }
+
+    @Override
+    public void remover(OrdemServico objeto) {
+
+    }
+
+    @Override
+    public List<OrdemServico> buscarPorNomeCliente(String nome) {
+        return null;
+    }
+}
