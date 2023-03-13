035bceba18a5645cc60505fba146450bcf66b6c1
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-03-13 16:37:51 -0300

    feat: Implementação Classe ServicoLista

--- PBL/src/main/java/dao/servico/ServicoLista.java
+++ PBL/src/main/java/dao/servico/ServicoLista.java
@@ -1,33 +1,68 @@
 package dao.servico;
 
+import model.OrdemServico;
 import model.Servico;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class ServicoLista implements ServicoDAO{
+    private List<Servico> listaServicos;
+    private int id;
+
+    public ServicoLista() {
+        this.listaServicos = new LinkedList<Servico>();
+        this.id = 0;
+    }
 
     @Override
     public void adicionarNaLista(Servico objeto) {
+        objeto.setId(this.id);
+        this.id++;
 
+        this.listaServicos.add(objeto);
     }
 
     @Override
     public Servico buscarPorId(int id) {
+        for (Servico servico: this.listaServicos){
+            if (servico.getId() == id)
+                return servico;
+        }
+
         return null;
     }
 
     @Override
     public List<Servico> buscarTodos() {
-        return null;
+        List<Servico> lista = new LinkedList<Servico>();
+
+        for (Servico servico: this.listaServicos){
+            lista.add(servico);
+        }
+
+        return lista;
     }
 
     @Override
     public void atualizar(Servico objeto) {
+        for (int i = 0; i < this.listaServicos.size(); i++){
+            if (this.listaServicos.get(i).getId() == objeto.getId()){
+                this.listaServicos.add(objeto.getId(), objeto);
 
+                return;
+            }
+        }
     }
 
     @Override
     public void remover(Servico objeto) {
+        for (int i = 0; i < this.listaServicos.size(); i++){
+            if (listaServicos.get(i).getId() == objeto.getId()){
+                this.listaServicos.remove(objeto);
 
+                return;
+            }
+        }
     }
 }
