6e67c47d4f5e8f4bfc997058b2f4f32f43b51eb2
Márcio Roberto <marcio.roberto0739@gmail.com>
2023-03-13 15:49:05 -0300

    feat: Implementação da classe OrdemServicoLista

--- PBL/src/main/java/dao/ordemServico/OrdemServicoLista.java
+++ PBL/src/main/java/dao/ordemServico/OrdemServicoLista.java
@@ -1,34 +1,71 @@
 package dao.ordemServico;
 
+import dao.DAO;
+import model.Cliente;
 import model.OrdemServico;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class OrdemServicoLista implements OrdemServicoDAO{
 
+    private List<OrdemServico> listaOrdens;
+    private int id;
+
+    public OrdemServicoLista() {
+        this.listaOrdens = new LinkedList<OrdemServico>();
+        this.id = 0;
+    }
+
     @Override
     public void adicionarNaLista(OrdemServico objeto) {
-        
+        objeto.setId(this.id);
+        this.id++;
+
+        this.listaOrdens.add(objeto);
     }
 
     @Override
     public OrdemServico buscarPorId(int id) {
+        for (OrdemServico ordem: this.listaOrdens){
+            if (ordem.getId() == id)
+                return ordem;
+        }
+
         return null;
     }
 
     @Override
     public List<OrdemServico> buscarTodos() {
-        return null;
+        List<OrdemServico> lista = new LinkedList<OrdemServico>();
+
+        for (OrdemServico ordem: this.listaOrdens){
+            lista.add(ordem);
+        }
+
+        return lista;
     }
 
     @Override
     public void atualizar(OrdemServico objeto) {
+        for (int i = 0; i < this.listaOrdens.size(); i++){
+            if (listaOrdens.get(i).getId() == objeto.getId()){
+                this.listaOrdens.add(objeto.getId(), objeto);
 
+                return;
+            }
+        }
     }
 
     @Override
     public void remover(OrdemServico objeto) {
+        for (int i = 0; i < this.listaOrdens.size(); i++){
+            if (listaOrdens.get(i).getId() == objeto.getId()){
+                this.listaOrdens.remove(objeto);
 
+                return;
+            }
+        }
     }
 
     @Override
